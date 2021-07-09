b3c4d0eaeb1310e1c4480c8d1e0d7ce73e5a77d8
Carla Valdivia <valdivia.carla@gmail.com>
2021-07-09 15:39:40 -0300

    Created the view

---
+++ view.rb
@@ -0,0 +1,26 @@
+class View
+
+  def ask_user_for_recipe_title
+    puts "What is the title of your recipe?"
+    print "> "
+    return gets.chomp
+  end
+
+  def ask_user_for_recipe_description
+    puts "What is the description of your recipe?"
+    print "> "
+    return gets.chomp
+  end
+
+  def ask_user_for_index
+    puts "Which recipe would you like to delete?"
+    print "> "
+    return gets.chomp.to_i - 1
+  end
+
+  def list_all_recipes(recipes)
+    recipes.each_with_index do |recipe, index|
+      puts "#{index + 1} - #{recipe.name} - #{recipe.description}"
+    end
+  end
+end
