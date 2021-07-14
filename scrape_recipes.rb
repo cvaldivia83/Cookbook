42cb6b5fc7b0c5025dd74d695b01542cdb4f2a4f
Carla Valdivia <valdivia.carla@gmail.com>
2021-07-14 09:33:52 -0300

    scraper updated

--- scrape_recipes.rb
+++ scrape_recipes.rb
@@ -1,7 +1,13 @@
 class ScrapeRecipes
+  attr_reader :title, :description, :rating, :url, :prep_time
+
   def initialize(ingredient)
     @ingredient = ingredient
-    @recipes_array = []
+    @title = []
+    @description = []
+    @rating = []
+    @url = []
+    @prep_time = ''
   end
 
   def call
@@ -10,32 +16,23 @@ class ScrapeRecipes
     html_doc = Nokogiri::HTML(html_file)
 
     html_doc.search('.card__detailsContainer"').each do |element|
-      title =  element.search('.card__title').first.text.strip
-      description = element.search('.card__summary').first.text.strip
-      rating = element.search('.review-star-text').text.strip.slice(/(\d.?\d*)/)
-      url = element.search('.card__titleLink').first.attribute('href').value
-
-      @recipes_array << {
-        title: title,
-        description: description,
-        rating: rating,
-        url: url
-      }
+      @title << element.search('.card__title').first.text.strip
+      @description << element.search('.card__summary').first.text.strip
+      @rating << element.search('.review-star-text').text.strip.slice(/(\d.?\d*)/)
+      @url << element.search('.card__titleLink').first.attribute('href').value
     end
-    @recipes_array
   end
 
   def get_prep_time(index)
-    html_file = URI.open(@recipes_array[index][:url]).read
+    html_file = URI.open(@url[index]).read
     doc = Nokogiri::HTML(html_file)
 
-    prep_time = ''
     doc.search('.recipe-meta-item').each do |item|
       if item.search('.recipe-meta-item-header').first.text.strip == 'total:'
-        prep_time = element.search('.recipe-meta-item-body').first.text.strip
+        @prep_time = element.search('.recipe-meta-item-body').first.text.strip
       end
     end
 
-    prep_time
+    @prep_time
   end
 end
